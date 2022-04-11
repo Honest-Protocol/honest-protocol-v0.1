1f6856f447b53cae1aa6507d4110a4622e983082
Kaili Wang <kailiwang@DNa808e1e.SUNet>
2022-04-10 22:38:09 -0700

    fixes to filter

--- contracts/HonestFilter.sol
+++ contracts/HonestFilter.sol
@@ -16,9 +16,29 @@ contract HonestFilter {
         console.log("Deploying FilterContract.");
     }
 
-    //  Called once by the factory at time of deployment
+    function isValidRequirement(uint256 mask, uint numLabels) private pure returns (bool) {
+        uint labelsUsed;
+        for (labelsUsed = 0; mask > 0; mask >>= 1) labelsUsed += 1;
+        return labelsUsed <= numLabels;
+    }
+
+    function valueReqsFitLabelReqs(uint256 _labelsRequired, uint256 _valuesRequired) private pure returns (bool) {
+        for (uint i = 1; i <= _labelsRequired && i <= _valuesRequired; i <<= 1) {
+            if ((i & _valuesRequired != 0) && // valueRequired is 1 for that label
+                (i & _labelsRequired == 0)) { //labelRequired is 0 for that label 
+                    return false;
+                }
+        }
+        return true;
+    }
+
+    //  Called by the factory at time of deployment
     function initialize(uint256 _labelsRequired, uint256 _valuesRequired, address _labelContract) external {
         require(msg.sender == factory, "FORBIDDEN");
+        uint allLabels = LabelContract(labelContract).getAllLabels().length;
+        require(isValidRequirement(_labelsRequired, allLabels), "labelsRequired must use existing labels");
+        require(isValidRequirement(_valuesRequired, allLabels), "valuesRequired must use existing labels");
+        require(valueReqsFitLabelReqs(_labelsRequired, _valuesRequired), "can only require a TRUE for a required label");
         labelsRequired = _labelsRequired;
         valuesRequired = _valuesRequired;
         labelContract = _labelContract;
@@ -33,14 +53,14 @@ contract HonestFilter {
         uint256 mask = 1;
         uint256 i = 0;
         while (i < allLabels.length) {
-            bool currRequired = _labelsRequired & 1 != 0;
+            bool currRequired = (_labelsRequired & 1) != 0;
             if (currRequired && 
                 //proof doesn't exist 
                 (i >= proofs.length || 
                     (keccak256(abi.encode(proofs[i])) == keccak256(abi.encode("")))
                 )
             ) {
-                    unknowns &= mask;
+                    unknowns |= mask;
             } 
 
             //prepare for next bit
